3fba6719a18b710165b24a1dce702c4871a8385a
edson <edson091990@gmail.com>
2024-09-01 17:30:18 -0300

    feat: create endpoint getProducts

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const productRoute = require('./routes/product.route.js');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-  res.send('test');
-});
+app.use('/api/products', productRoute);
 
 app.listen(port, async () => {
   console.log(`Connection running on port ${port}`);
