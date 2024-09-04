5c814edf6a5ddd72dd9fcb0bef838936ba8c5c29
edson <edson091990@gmail.com>
2024-09-04 00:23:34 -0300

    feat: create endpoint getProduct

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/product.model.js');
-const {getProducts} = require('../controllers/product.controller.js');
+const {getProducts, getProduct} = require('../controllers/product.controller.js');
 
 router.get('/', getProducts);
+router.get('/:id', getProduct);
 
 module.exports = router;
